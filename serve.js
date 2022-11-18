63d913170f85cd05c133c548c809c5bd587aad45
陆雯旭 <992273307@qq.com>
2022-11-18 14:13:27 +0800

    bugfix

--- serve.js
+++ serve.js
@@ -19,10 +19,14 @@ if (!Object.prototype.hasOwnProperty.call(values, 'root')) {
 // Require the framework and instantiate it
 const fastify = require('fastify')({ logger: true })
 
+fastify.register(require('@fastify/static'), {
+  root: values.root,
+})
+
 // Run the server!
 const start = async () => {
   try {
-    await fastify.listen({ port: 3000 })
+    await fastify.listen({ port: 3000, host: '0.0.0.0' })
   } catch (err) {
     fastify.log.error(err)
     process.exit(1)
@@ -30,7 +34,6 @@ const start = async () => {
 }
 
 (async () => {
-  const { fileTypeFromBuffer } = await import('file-type')
   fastify.get('/api/file', async (request, reply) => {
     const { path = '' } = request.query
     const abp = normalize(`${values.root}/${path}`)
@@ -49,7 +52,13 @@ const start = async () => {
           JSON.stringify(
             files
               .filter(file => !file.name.startsWith('.'))
-              .sort((a, b) => a.name.localeCompare(b.name))
+              .sort((a, b) => {
+                const aIsDir = a.isDirectory(), bIsDir = b.isDirectory()
+                if (aIsDir === bIsDir) {
+                  return a.name.localeCompare(b.name)
+                }
+                return aIsDir ? -1 : 1
+              })
               .map(file => ({ name: file.name, isDir: file.isDirectory() })),
           ),
           (err, buffer) => {
@@ -59,12 +68,13 @@ const start = async () => {
         )
       }))
     } else {
-      const buffer = await readFile(abp)
-      const data = await fileTypeFromBuffer(buffer);
-      if (data) {
-        reply.header('Content-Type', data.mime)
-      }
-      reply.send(buffer)
+      // const buffer = await readFile(abp)
+      // const data = await fileTypeFromBuffer(buffer);
+      // if (data) {
+      //   reply.header('Content-Type', data.mime)
+      // }
+      // reply.send(buffer)
+      return reply.sendFile(path)
     }
   })
 
